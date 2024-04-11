fcc1d33bd2db69dd430fe14e3013d5f2caf9522e
Jackson <jacksondstaples1@gmail.com>
2024-04-10 21:21:17 -0700

    yup

---
+++ CS 121 Project #4/Solver.cpp
@@ -0,0 +1,10 @@
+/* Solver.cpp
+ *
+ * CS 121.Bolden...........Jackson Staples
+ * 4/10/2024 .................stap2053@vandals.uidaho.edu
+ *
+ * Program to take mazes from text files and Find a path through them
+ * using a queue and dynamic arrays.
+ *---------------------------------------------------------------------
+ */
+
---
+++ CS 121 Project #4/queue.h
@@ -0,0 +1,20 @@
+/* queue.h
+ *
+ * CS 121.Bolden...........Jackson Staples
+ * 4/10/2024 .................stap2053@vandals.uidaho.edu
+ *
+ * Header file 
+ *---------------------------------------------------------------------
+*/
+
+
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include <cstddef>
+
+class queue
+{
+    public:
+    
+};
